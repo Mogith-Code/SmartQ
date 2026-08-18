9725b6b15228472c7b940e1b19051ee42bee78c2
Mogith-Code <kmogith1@gmail.com>
2026-08-18 23:47:02 +0530

    front added

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/theme/app_theme.dart';
+import 'features/auth/presentation/screens/login_screen.dart';
+
